22d4ced36a11c017f1e16eb497f8c00685d6a5cd
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-08-28 01:14:01 +0600

    Post data

---
+++ post.js
@@ -0,0 +1,25 @@
+const url = 'https://jsonplaceholder.typicode.com/posts';
+
+function loadPosts() {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayPost(data))
+}
+
+function displayPost(posts) {
+    const postsContainer = document.getElementById('post-container');
+    for (const post of posts) {
+        console.log(post);
+        const postsDiv = document.createElement('div');
+        postsDiv.classList.add('post');
+        postsDiv.innerHTML = `
+        <h4>${post.userId}</h4>
+        <h5>Title: ${post.title}</h5>
+        <p>Description: ${post.body}</p>
+        `;
+        postsContainer.appendChild(postsDiv);
+    }
+}
+
+loadPosts();
+
