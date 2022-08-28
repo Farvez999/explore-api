58e8741339ca4a8eb1957c667c2d961ee462bdc8
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-08-29 01:13:34 +0600

    Quote Api Json

--- post.js
+++ post.js
@@ -1,25 +1,28 @@
-const url = 'https://jsonplaceholder.typicode.com/posts';
+// const url = 'https://jsonplaceholder.typicode.com/posts';
 
-function loadPosts() {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => displayPost(data))
-}
+// function loadPosts() {
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(data => displayPost(data))
+// }
 
-function displayPost(posts) {
-    const postsContainer = document.getElementById('post-container');
-    for (const post of posts) {
-        console.log(post);
-        const postsDiv = document.createElement('div');
-        postsDiv.classList.add('post');
-        postsDiv.innerHTML = `
-        <h4>${post.userId}</h4>
-        <h5>Title: ${post.title}</h5>
-        <p>Description: ${post.body}</p>
-        `;
-        postsContainer.appendChild(postsDiv);
-    }
-}
+// function displayPost(posts) {
+//     const postsContainer = document.getElementById('post-container');
+//     for (const post of posts) {
+//         console.log(post);
+//         const postsDiv = document.createElement('div');
+//         postsDiv.classList.add('post');
+//         postsDiv.innerHTML = `
+//         <h4>${post.userId}</h4>
+//         <h5>Title: ${post.title}</h5>
+//         <p>Description: ${post.body}</p>
+//         `;
+//         postsContainer.appendChild(postsDiv);
+//     }
+// }
 
-loadPosts();
+// loadPosts();
+
+const stuInfo = JSON.stringify({ name: "James", roll: 3 });
+console.log(stuInfo.name);
 
---
+++ quote.js
@@ -0,0 +1,12 @@
+const loadQuote = () => {
+    fetch('https://api.kanye.rest/')
+        .then(res => res.json())
+        .then(data => displyQuote(data))
+}
+
+const displyQuote = quote => {
+    const blockQuote = document.getElementById('quote');
+    blockQuote.innerText = quote.quote;
+    console.log(quote);
+}
+
