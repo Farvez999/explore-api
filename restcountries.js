429831c52b363586dd27981cb6ffe61f76d17d69
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-08-29 22:28:01 +0600

    Rest Country Api button details

--- restcountries.js
+++ restcountries.js
@@ -1,4 +1,4 @@
-const randomUser = () => {
+const loadCountry = () => {
     fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data => userDisplay(data));
@@ -26,10 +26,28 @@ const userDisplay = cuntrices => {
         <h3>Cuntry Name : ${cuntrice.name.common ? cuntrice.name.common : 'No Country'}</h3>
         <h4>Capital : ${cuntrice.capital ? cuntrice.capital[0] : 'No Capital'}</h4>
         <p>Borders : ${cuntrice.borders ? cuntrice.borders[0] : 'No Borders'}</p>
+        <button onclick="loadCountryDetails('${cuntrice.cca2}')">Ditails</button>
         `;
         countriesContainer.appendChild(divCountries);
     })
     console.log(cuntrices)
 }
 
-randomUser();
+const loadCountryDetails = (code) => {
+    //https://restcountries.com/v2/alpha/{code}
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountry(data[0]))
+}
+
+const displayCountry = country => {
+    console.log(country)
+    const countryDetails = document.getElementById('country-details');
+    countryDetails.innerHTML = `
+    <h2>Country Name: ${country.name.common}</h2>
+    <img src="${country.flags.png}">
+    `;
+}
+
+loadCountry();
